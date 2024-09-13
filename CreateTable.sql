855a8233c4e5bcdbc3c45ada1005257419cbad39
IamMax-105 <141610003+IamMax-105@users.noreply.github.com>
2024-09-13 14:27:37 +0700

    Update CreateTable.sql

--- CreateTable.sql
+++ CreateTable.sql
@@ -1,5 +1,5 @@
 CREATE TABLE PUPIL (
-    PupilID CHAR(9) PRIMARY KEY CHECK (PupilID LIKE 'P_____'),
+    PupilID NVARCHAR2(9) PRIMARY KEY,
     PupilName NVARCHAR2(30) NOT NULL,
     ClassID CHAR(5) NOT NULL,
     DateOfBirth DATE NOT NULL,
@@ -13,18 +13,20 @@ CREATE TABLE PUPIL (
     Hometown NVARCHAR2(90),
     Religion NVARCHAR2(30),
     PupilCardID CHAR(10) NOT NULL,
-    FOREIGN KEY (ClassID) REFERENCES CLASSES(ClassID)
+    FOREIGN KEY (ClassID) REFERENCES CLASSES(ClassID),
+    CONSTRAINT chk_PupilID CHECK (PupilID LIKE 'P_____')
 );
 
 CREATE TABLE SCHOOL_BOARD (
-    BoardID CHAR(5) PRIMARY KEY CHECK (BoardID LIKE 'B___'),
+    BoardID NVARCHAR2(5) PRIMARY KEY,
     BoardName NVARCHAR2(30) NOT NULL,
-    School_Position CHAR(50) NOT NULL
+    School_Position CHAR(50) NOT NULL,
+    CONSTRAINT chk_BoardID CHECK (BoardID LIKE 'B___')
 );
 
 CREATE TABLE TEACHER_REWARDS (
-    BoardID CHAR(5) NOT NULL,
-    TeacherID CHAR(5) NOT NULL,
+    BoardID NVARCHAR2(5) NOT NULL,
+    TeacherID NVARCHAR2(5) NOT NULL,
     Semester NUMBER(1) NOT NULL,
     SchoolYear NUMBER(5) NOT NULL,
     FOREIGN KEY (BoardID) REFERENCES SCHOOL_BOARD(BoardID),
@@ -35,39 +37,47 @@ CREATE TABLE TEACHER_REWARDS (
 
 
 CREATE TABLE PUPIL_REWARDS (
-    BoardID CHAR(5) CHECK (BoardID LIKE 'B___') NOT NULL,
-    PupilID CHAR(9) CHECK (PupilID LIKE 'P_____') NOT NULL,
+    BoardID NVARCHAR2(5) NOT NULL,
+    PupilID NVARCHAR2(9) NOT NULL,
     Semester NUMBER(1) NOT NULL,
     SchoolYear NUMBER(5) NOT NULL,
     AwardTitle NVARCHAR2(30) NOT NULL,
     FOREIGN KEY (BoardID) REFERENCES SCHOOL_BOARD(BoardID),
-    FOREIGN KEY (PupilID) REFERENCES PUPIL(PupilID)
+    FOREIGN KEY (PupilID) REFERENCES PUPIL(PupilID),
+    CONSTRAINT chk_BoardID CHECK (BoardID LIKE 'B___'), 
+    CONSTRAINT chk_PupilID CHECK (PupilID LIKE 'P_____') 
 );
 
 
 CREATE TABLE ACADEMIC_RESULTS (
-    ResultID CHAR(9) PRIMARY KEY CHECK (ResultID LIKE 'R___'),
-    PupilID CHAR(9) CHECK (PupilID LIKE 'P_____'),
+    ResultID NVARCHAR2(9) PRIMARY KEY ,
+    PupilID NVARCHAR2(9) PRIMARY KEY,
     AverageScore_Semester1 NUMBER(1) NOT NULL,
     AverageScore_Semester2 NUMBER(5) NOT NULL,
     FinalAverageScore NVARCHAR2(30) NOT NULL,
-    Conduct NVARCHAR2(10) NOT NULL check (Conduct IN ('Excellent', 'Good', 'Average')),
+    Conduct NVARCHAR2(10) NOT NULL,
     AcademicPerformance NVARCHAR2(10) NOT NULL,
-    PromotionStatus NVARCHAR2(1) CHECK (PromotionStatus IN ('Y', 'N')),
-    FOREIGN KEY (PupilID) REFERENCES PUPIL(PupilID)
+    PromotionStatus NVARCHAR2(1),
+    FOREIGN KEY (PupilID) REFERENCES PUPIL(PupilID),
+    CONSTRAINT chk_PromotionStatus CHECK (PromotionStatus IN ('Y', 'N')),
+    CONSTRAINT chk_Conduct CHECK (Conduct IN ('Excellent', 'Good', 'Average', 'Poor')),
+    CONSTRAINT chk_ResultID CHECK (ResultID LIKE 'R___'),
+    CONSTRAINT chk_PupilID CHECK (PupilID LIKE 'P_____')
 );
 
 
 CREATE TABLE TEACHER (
-    TeacherID CHAR(5) PRIMARY KEY CHECK (TeacherID LIKE 'T___'),
+    TeacherID NVARCHAR2(5) PRIMARY KEY,
     TeacherName NVARCHAR2(30),
     Address NVARCHAR2(90),
-    Specialization CHAR(2)CHECK (Specialization IN ('Sc', 'So'))
+    Specialization CHAR(2),
+    CONSTRAINT chk_TeacherID CHECK (TeacherID LIKE 'T___'),
+    CONSTRAINT chk_Specialization CHECK (Specialization IN ('Sc', 'So'))
 );
 
 
 CREATE TABLE CLASSES (
-    ClassID CHAR(5) PRIMARY KEY ,
+    ClassID NVARCHAR2(5) PRIMARY KEY ,
     ClassName NVARCHAR2(30),
     ClassSize NUMBER(3),
     CONSTRAINT chk_CL_ClassID CHECK (ClassID LIKE 'C___')
@@ -75,7 +85,7 @@ CREATE TABLE CLASSES (
 
 
 CREATE TABLE TEACHING_ASSIGNMENT (
-    TeacherID CHAR(5) PRIMARY KEY,
+    TeacherID NVARCHAR2(5) PRIMARY KEY,
     SubjectID CHAR(5),
     ClassID CHAR(5),
     CONSTRAINT chk_TA_TeacherID CHECK (TeacherID LIKE 'T___')
